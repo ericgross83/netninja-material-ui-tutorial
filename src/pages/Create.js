36748e327dd58e3692e0e78f27929734a3709eec
Eric Gross <eric@vw-dilab.com>
2022-05-13 16:23:13 +0200

    Lesson code

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,39 +1,90 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import Container from '@material-ui/core/Container'
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
-import { makeStyles } from '@material-ui/core'
+import {makeStyles, TextField} from '@material-ui/core'
 
 const useStyles = makeStyles({
+    field: {
+        marginBottom: 20,
+        marginTop: 20,
+        display: "block"
+    }
 })
 
 export default function Create() {
-  const classes = useStyles()
+    const classes = useStyles()
 
-  return (
-    <Container size="sm">
-      <Typography
-        className={classes.title}
-        variant="h6" 
-        color="textSecondary"
-        component="h2"
-        gutterBottom
-      >
-        Create a New Note
-      </Typography>
+    const [title, setTitle] = useState('')
+    const [details, setDetails] = useState('')
+    const [titleError, setTitleError] = useState(false)
+    const [detailsError, setDetailsError] = useState(false)
 
-      <Button
-        className={classes.btn}
-        onClick={() => console.log('you clicked me')}
-        type="submit" 
-        color="secondary"
-        variant="contained"
-        endIcon={<KeyboardArrowRightIcon />}>
-        Submit
-      </Button>
+    const handleSubmit = (e) => {
+        setTitleError(false)
+        setDetailsError(false)
 
-      
-    </Container>
-  )
+        if(title === '') {
+            setTitleError(true)
+        }
+
+        if(details === '') {
+            setDetailsError(true)
+        }
+
+        e.preventDefault()
+        if (title && details) {
+            console.log(title, details)
+        }
+    }
+
+    return (
+        <Container size="sm">
+            <Typography
+                className={classes.title}
+                variant="h6"
+                color="textSecondary"
+                component="h2"
+                gutterBottom
+            >
+                Create a New Note
+            </Typography>
+
+            <form noValidate autoComplete={'off'} onSubmit={handleSubmit}>
+                <TextField
+                    onChange={(e) => setTitle(e.target.value)}
+                    className={classes.field}
+                    label={'Note Title'}
+                    variant={"outlined"}
+                    color={"secondary"}
+                    fullWidth
+                    required
+                    error={titleError}
+                />
+                <TextField
+                    onChange={(e) => setDetails(e.target.value)}
+                    className={classes.field}
+                    label='Details'
+                    variant={"outlined"}
+                    color={"secondary"}
+                    multiline
+                    rows={4}
+                    fullWidth
+                    required
+                    error={detailsError}
+                />
+                <Button
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                    endIcon={<KeyboardArrowRightIcon/>}>
+                    Submit
+                </Button>
+            </form>
+
+
+
+        </Container>
+    )
 }
