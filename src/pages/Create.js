dada400e2cf7aef04726ae5aab269c75b8caf406
Eric Gross <eric@vw-dilab.com>
2022-05-13 15:39:34 +0200

    A new beginning

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,37 +1,50 @@
 import React from 'react'
-import {Button, Container, Typography} from "@mui/material";
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Container from '@material-ui/core/Container'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+import { makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+      background: 'blue'
+    },
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20,
+  }
+})
 
 export default function Create() {
-    return (
-        <Container>
-            <Typography
-                variant={"h6"}
-                component={"h2"}
-                gutterBottom
-                color={"textSecondary"}
-            >
-                Create a New Note
-            </Typography>
+  const classes = useStyles()
 
-            <Button
-                color={"secondary"}
-                variant={"contained"}
-                onClick={() => console.log('you clicked me')}
-                endIcon={<KeyboardArrowRightIcon/>}
-            >
-                Submit
-            </Button>
+  return (
+    <Container size="sm">
+      <Typography
+        className={classes.title}
+        variant="h6" 
+        color="textSecondary"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
 
+      <Button
+        className={classes.btn}
+        onClick={() => console.log('you clicked me')}
+        type="submit" 
+        color="secondary" 
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}>
+        Submit
+      </Button>
 
-            {/*Icons*/}
-            <br/>
-            {/*<AcUnitOutlinedIcon/>*/}
-            {/*<AcUnitOutlinedIcon color={"secondary"} fontSize={"large"}/>*/}
-            {/*<AcUnitOutlinedIcon color={"secondary"} fontSize={"small"}/>*/}
-            {/*<AcUnitOutlinedIcon color={"action"} fontSize={"small"}/>*/}
-            {/*<AcUnitOutlinedIcon color={"error"} fontSize={"small"}/>*/}
-            {/*<AcUnitOutlinedIcon color={"disabled"} fontSize={"small"}/>*/}
-        </Container>
-    )
+      
+    </Container>
+  )
 }
