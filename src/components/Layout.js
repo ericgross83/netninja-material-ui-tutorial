439c7377d2afdf98ff868ec95043c7e70fcbaa0a
Eric Gross <eric@vw-dilab.com>
2022-05-20 21:58:54 +0200

    Lesson code

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {Drawer, makeStyles, Typography} from "@material-ui/core";
+import {Drawer, List, ListItem, ListItemIcon, ListItemText, makeStyles, Typography} from "@material-ui/core";
+import {AddCircleOutlined, SubjectOutlined} from "@material-ui/icons";
+import {useHistory, useLocation} from "react-router-dom";
 
 const drawerWidth = 240
 
@@ -16,11 +18,38 @@ const useStyles = makeStyles({
     },
     root: {
         display: "flex"
+    },
+    active: {
+        background: '#f4f4f4'
     }
 })
 
+
 export default function Layout({children}) {
+
     const classes = useStyles()
+    const history = useHistory()
+    const location = useLocation()
+
+    function handleItemClick(path) {
+        history.push(path)
+
+    }
+
+
+    const menuItems = [
+        {
+            text: 'My Notes',
+            icon: <SubjectOutlined color={"secondary"}/>,
+            path: '/'
+        },
+        {
+            text: 'Create Note',
+            icon: <AddCircleOutlined color={"secondary"}/>,
+            path: '/create'
+        },
+    ]
+
     return (
         <div className={classes.root}>
 
@@ -29,13 +58,29 @@ export default function Layout({children}) {
                 className={classes.drawer}
                 variant={"permanent"}
                 anchor={"left"}
-                classes={{ paper: classes.drawerPaper}}
+                classes={{paper: classes.drawerPaper}}
             >
                 <div>
                     <Typography variant={"h5"}>
                         Ninja Notes
                     </Typography>
                 </div>
+
+                <List>
+                    {menuItems.map(item => (
+                        <ListItem key={item.text}
+                                  button
+                                  onClick={() => handleItemClick(item.path)}
+                                  className={location.pathname === item.path ? classes.active : null}
+                        >
+                            <ListItemIcon>
+                                {item.icon}
+                            </ListItemIcon>
+                            <ListItemText primary={item.text}/>
+                        </ListItem>
+                    ))}
+                </List>
+
             </Drawer>
 
             {/* side drawer */}
